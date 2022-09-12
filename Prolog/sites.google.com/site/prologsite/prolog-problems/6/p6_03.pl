8ef545c6688d6978ff10b4c4c4deda095b9bbd18
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2022-09-12 16:04:34 -0300

    Add files via upload

---
+++ Prolog/sites.google.com/site/prologsite/prolog-problems/6/p6_03.pl
@@ -0,0 +1,11 @@
+% 6.03 (*) Cycle from a given node
+
+% cycle(G,A,P) :- P is a closed path starting at node A in the graph G.
+%    G is given in graph-term form.
+%    (+,+,?)
+
+:- ensure_loaded(p6_01).  % conversions
+:- ensure_loaded(p6_02).  % adjacent/3 and path/4
+
+cycle(G,A,P) :- 
+   adjacent(B,A,G), path(G,A,B,P1), length(P1,L), L > 2, append(P1,[A],P).
---
+++ Prolog/sites.google.com/site/prologsite/prolog-problems/6/p6_05.pl
@@ -0,0 +1,31 @@
+% 6.05 (**) Construct the minimal spanning tree of a labelled graph 
+
+% ms_tree(G,T,S) :- T is a minimal spanning tree of the graph G.
+%    S is the sum of the edge values. Prim's algorithm.
+%    (graph-term graph-term) (+,?)
+
+:- ensure_loaded(p6_01).  % conversions
+:- ensure_loaded(p6_04).  % transfer/3, incident/3, and accept/3
+
+ms_tree(graph([N|Ns],GraphEdges),graph([N|Ns],TreeEdges),Sum) :- 
+   predsort(compare_edge_values,GraphEdges,GraphEdgesSorted),
+   transfer(Ns,GraphEdgesSorted,TreeEdgesUnsorted),
+   sort(TreeEdgesUnsorted,TreeEdges),
+   edge_sum(TreeEdges,Sum).
+
+compare_edge_values(Order,e(X1,Y1,V1),e(X2,Y2,V2)) :- 
+	compare(Order,V1+X1+Y1,V2+X2+Y2).
+
+edge_sum([],0).
+edge_sum([e(_,_,V)|Es],S) :- edge_sum(Es,S1), S is S1 + V. 
+
+% Example graph p6_05.dat
+
+test :-  
+   see('p6_05.dat'), read(G), seen,
+   human_gterm(H,G),
+   write(H), nl, 
+   ms_tree(G,T,S),
+   human_gterm(TH,T),
+   write(S), nl,
+   write(TH).
